0cf4b03a77d2490c7412ca16a320d5a5edc6fc93
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-09-21 14:09:41 -0400

    44 - Start

--- solar/src/App.vue
+++ solar/src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container-fluid">
+            <div class="row title-row">
+              CEILING
+            </div>
+            <div class="row button-row">
+                <Button msg="Turn Light On" color="rgb(0, 100, 0)"/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Button from './components/Button.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Button
+        },
+        data: function () {
+            return {}
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 20px;
+    }
+
+    .button-row {
+        padding: 30px;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        body {
+            background-color: #353535;
+            color: white;
+        }
+        #app {
+          color: white;
+        }
+    }
 </style>
---
+++ solar/src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-12 col-lg-6 button" v-bind:style="{ 'background-color': color }">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Button.vue",
+        props: {
+            msg: String,
+            color: String
+        }
+    }
+</script>
+
+<style scoped>
+    .button {
+        border-radius: 10px;
+        background-color: black;
+        color: white;
+        font-size: 20px;
+    }
+
+    .col-12 {
+        padding: 40px;
+    }
+</style>
